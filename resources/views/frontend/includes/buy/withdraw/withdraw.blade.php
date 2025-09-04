2e40bab8ad4e70a4bf5a5fcfbe6af799eba9def6
ashad5698-dev <ashad.dev@blockversesolutions.com>
2025-09-04 10:48:28 +0600

    deposit and withdraw sub menu

---
+++ resources/views/frontend/includes/buy/withdraw/withdraw.blade.php
@@ -0,0 +1,13 @@
+<div class="tab-pane fade" id="withdraw" role="tabpanel" aria-labelledby="withdraw-tab">
+    <div class="withdraw-content position-relative">
+        @include('frontend.includes.buy.withdraw.withdraw_form')
+
+        <!-- How to buy Area End  -->
+        @include('frontend.includes.buy.withdraw.why_bcoin_mart')
+        <!-- How to buy Area End  -->
+
+        <!-- Coin pair Start  -->
+        @include('frontend.includes.buy.popular_crypto')
+        <!-- Coin pair End  -->
+    </div>
+</div>
