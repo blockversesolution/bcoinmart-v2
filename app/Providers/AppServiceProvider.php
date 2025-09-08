9ada64b0a7c8e78f0d6536a3263678cf6acc0612
ashad5698-dev <ashad.dev@blockversesolutions.com>
2025-09-08 15:47:07 +0600

    location and context add in provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use Jenssegers\Agent\Agent;
+use Spatie\Activitylog\Models\Activity;
+use Stevebauman\Location\Facades\Location;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +26,32 @@ class AppServiceProvider extends ServiceProvider
         Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
             $event->extendSocialite('telegram', \SocialiteProviders\Telegram\Provider::class);
         });
+
+        Activity::saving(function (Activity $activity) {
+            $agent = app(Agent::class);
+
+            // Context
+            $activity->context = json_encode([
+                'ip'               => request()->ip(),
+                'user_agent'       => request()->userAgent(),
+                'device'           => $agent->device(),
+                'platform'         => $agent->platform(),
+                'platform_version' => $agent->version($agent->platform()),
+                'browser'          => $agent->browser(),
+                'browser_version'  => $agent->version($agent->browser()),
+                'timestamp'        => now()->format('Y-m-d H:i:s'),
+            ]);
+
+            // Location
+            $location = Location::get(request()->ip());
+            $activity->location = json_encode([
+                'ip'           => $location?->ip ?? request()->ip(),
+                'country'      => $location?->countryName ?? null,
+                'country_code' => $location?->countryCode ?? null,
+                'city'         => $location?->cityName ?? null,
+                'region'       => $location?->regionName ?? null,
+            ]);
+        });
+
     }
 }
